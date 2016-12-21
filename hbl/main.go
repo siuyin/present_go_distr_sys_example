5da5bb58fb7869c15b6b821f6d5fbb65084d5585
Loh Siu Yin <siuyin@beyondbroadcast.com>
2016-12-22 01:25:45 +0800

    Added in memory database for hbl.

--- hbl/main.go
+++ hbl/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"sort"
 	"time"
 
 	"siuyin/junk/nats/exampleA/cfg"
@@ -9,8 +11,9 @@ import (
 	"github.com/nats-io/go-nats"
 )
 
-type nrs struct {
-	Name, Rank, ID string
+type dat struct {
+	cfg.NRS
+	T time.Time
 }
 
 func main() {
@@ -23,11 +26,14 @@ func main() {
 		c.Request(cfg.IDOffice, "May I an ID please?", &id, time.Second)
 	}
 	me := cfg.NRS{Name: "Herbert", Rank: cfg.HBListener, ID: id}
+	seen := map[string]dat{}
 	//010_OMIT
 	log.Println("Listener Starting...")
 
-	c.Subscribe(cfg.HeartBeat, func(agent *nrs) {
-		log.Printf("%v", agent)
+	c.Subscribe(cfg.HeartBeat, func(agent *dat) {
+		agent.T = time.Now()
+		key := agent.Name + agent.Rank
+		seen[key] = *agent
 	})
 	//020_OMIT
 
@@ -35,6 +41,22 @@ func main() {
 		select {
 		case <-tkr:
 			c.Publish(cfg.HeartBeat, me)
+			displayDat(&seen)
 		}
+	}
+}
+
+func displayDat(d *map[string]dat) {
+	keys := make([]string, len(*d))
+	i := 0
+	for k := range *d {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println("=================================")
+	for _, k := range keys {
+		v := (*d)[k]
+		fmt.Printf("%s %s %s\n", v.Name, v.Rank, v.T.Format("15:04:05 MST"))
 	}
 }
