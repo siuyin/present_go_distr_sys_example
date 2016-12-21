b576fcc7889e9ec1ef39df65201c8d1104a953ef
Loh Siu Yin <siuyin@beyondbroadcast.com>
2016-12-22 01:49:12 +0800

    Added mutex to guard access to hbl's map.

--- hbl/main.go
+++ hbl/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"sort"
+	"sync"
 	"time"
 
 	"siuyin/junk/nats/exampleA/cfg"
@@ -15,6 +16,8 @@ type dat struct {
 	cfg.NRS
 	T time.Time
 }
+
+var mtx sync.Mutex
 
 func main() {
 	nc, _ := nats.Connect(nats.DefaultURL)
@@ -33,7 +36,9 @@ func main() {
 	c.Subscribe(cfg.HeartBeat, func(agent *dat) {
 		agent.T = time.Now()
 		key := agent.Name + agent.Rank
+		mtx.Lock()
 		seen[key] = *agent
+		mtx.Unlock()
 	})
 	//020_OMIT
 
@@ -49,14 +54,24 @@ func main() {
 func displayDat(d *map[string]dat) {
 	keys := make([]string, len(*d))
 	i := 0
+
+	mtx.Lock()
 	for k := range *d {
 		keys[i] = k
 		i++
 	}
+	mtx.Unlock()
+
 	sort.Strings(keys)
 	fmt.Println("=================================")
 	for _, k := range keys {
+		mtx.Lock()
 		v := (*d)[k]
-		fmt.Printf("%s %s %s\n", v.Name, v.Rank, v.T.Format("15:04:05 MST"))
+		mtx.Unlock()
+		s := ""
+		if time.Now().Sub(v.T).Seconds() > 2 {
+			s = "F: "
+		}
+		fmt.Printf("%s%s %s %s\n", s, v.Name, v.Rank, v.T.Format("15:04:05 MST"))
 	}
 }
