a3e07ef9d0152385d098605c785276b8f2ac599d
Loh Siu Yin <siuyin@beyondbroadcast.com>
2016-12-30 13:48:04 +0800

    Tidy visualisation.js.

--- hbl/public/js/visualisation.js
+++ hbl/public/js/visualisation.js
@@ -1,48 +1,26 @@
-
-
-function formatDateTime(t) {
-  return t.getFullYear() + "-" +
-    d3.format("02d")(t.getMonth()) + "-" +
-    d3.format("02d")(t.getDate()) + "_" +
-    d3.format("02d")(t.getHours()) + ":" +
-    d3.format("02d")(t.getMinutes()) +":" +
-    d3.format("02d")(t.getSeconds());
-}
+// Global variables.
+var nodeRadius=12;
 
 var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+  width = +svg.attr("width"),
+  height = +svg.attr("height")
+;
 
 // define arrow markers for graph links
 svg.append('svg:defs').append('svg:marker')
-    .attr('id', 'end-arrow')
-    .attr('viewBox', '0 -5 10 10')
-    .attr('refX', 6)
-    .attr('markerWidth', 3)
-    .attr('markerHeight', 3)
-    .attr('orient', 'auto')
+  .attr('id', 'end-arrow')
+  .attr('viewBox', '0 -5 10 10')
+  .attr('refX', 6)
+  .attr('markerWidth', 3)
+  .attr('markerHeight', 3)
+  .attr('orient', 'auto')
   .append('svg:path')
-    .attr('d', 'M0,-5L10,0L0,5');
-        // .attr('fill', '#000');
+  .attr('d', 'M0,-5L10,0L0,5')
+  // .attr('fill', '#000')
+;
 
-// var color = d3.scaleOrdinal(d3.schemeCategory10);
-var color = function(group){
-  switch(group){
-    case "live":
-      return d3.schemeCategory10[2];
-      break;
-    case "liveManager":
-      return d3.schemeCategory10[1];
-      break;
-    case "board":
-      return d3.schemeCategory10[9];
-      break;
-    default:
-      return d3.schemeCategory10[3];
-  }
-}
-
-let linkDistance = function(l){
+// force atlas simulation related.
+function linkDistance(l){
   switch(l.value){
     case "live":
     case "liveManager":
@@ -55,20 +33,40 @@ let linkDistance = function(l){
       return 300;
   }
 }
-
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+}
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; })
       .distance(function(d){return linkDistance(d)}))
     .force("charge", d3.forceManyBody().strength(-75))
     .force("yPos", d3.forceY(height/2).strength(0.03)) // useful for landscape
     //.force("xPos", d3.forceX(width/2).strength(0.03)) // useful for portrait
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    ;
 
-var nodeRadius=12;
+
+// update date time on html page
+//d3.select("#updateTime").html(new Date());
+
+// d3 calls the heartbeat API endpoint.
 d3.json("/heartbeat", function(error, dat) {
+
   var nodes=[], links=[], boards={};
+  var now = new Date();
+
   let liveNess = function(n){
-    let now = new Date();
     if (now.getTime() - (new Date(n.T).getTime()) > 2000){
       return "dead";
     } else {
@@ -77,18 +75,72 @@ d3.json("/heartbeat", function(error, dat) {
       }
       return "live";
     }
-  }
+  };
+
   let linkType = function(source,status){
     if (source == "EgA.IDOffice" &&
       (status == "live" || status == "liveManager")) {
       return "IDOffice";
     }
     return status;
-  }
+  };
 
+  let strokeWidth = function(l) {
+    switch(l.value){
+      case "dead":
+        return 0.3;
+      case "IDOffice":
+        return 0.3;
+      default:
+        return 2;
+    }
+  };
+
+  let dispName = function(d){
+    if (d.name !== undefined) {
+      if (d.group == "dead") {
+        return d.name+"#died:"+formatDateTime(d.t);
+      } else {
+        return d.name;
+      }
+    } else {
+      return d.id;
+    }
+  };
+
+  // color returns a color from a pallete depending on the
+  // value of group.
+  let color = function(group){
+    switch(group){
+      case "live":
+        return d3.schemeCategory10[2];
+        break;
+      case "liveManager":
+        return d3.schemeCategory10[1];
+      break;
+      case "board":
+        return d3.schemeCategory10[9];
+        break;
+      default:
+        return d3.schemeCategory10[3];
+    }
+  };
+
+  let formatDateTime = function(t) {
+    return t.getFullYear() + "-" +
+      d3.format("02d")(t.getMonth()) + "-" +
+      d3.format("02d")(t.getDate()) + "_" +
+      d3.format("02d")(t.getHours()) + ":" +
+      d3.format("02d")(t.getMinutes()) +":" +
+      d3.format("02d")(t.getSeconds());
+  };
+
+
+  // update function proper starts here -------------------------------
   if (error) throw error;
+
+  // create  nodes,links and boards objects
   for (var key in dat) {
-    let now = new Date();
     let status = liveNess(dat[key]);
 
     nodes.push({"id":dat[key].ID,"name":dat[key].Name,
@@ -108,26 +160,12 @@ d3.json("/heartbeat", function(error, dat) {
     }
   }
   for (var key in boards){
-    // var gp = Math.floor(Math.random()*19)+2
-    // var gp = Math.abs(key.hashCode())%18+2
-    var gp="board";
-    nodes.push({"id":key,"group":gp});
-    // console.log(key, gp)
+    nodes.push({"id":key,"group":"board"});
   }
   var graph={"nodes":nodes,"links":links};
 
-  let strokeWidth = function(l) {
-    switch(l.value){
-      case "dead":
-        return 0.3;
-      case "IDOffice":
-        return 0.3;
-      default:
-        return 2;
-    }
-  }
 
-
+  // draw node circles
   var nodec = svg.append("g")
       .attr("class", "nodes")
     .selectAll("circle")
@@ -143,6 +181,7 @@ d3.json("/heartbeat", function(error, dat) {
 
 
   /*
+   Draw link paths.
    Let Vector V = P2 - P1, where P1 is the start position Vector
    and P2 the end position vector.
    And let r be the node radius of the start and end nodes.
@@ -159,28 +198,8 @@ d3.json("/heartbeat", function(error, dat) {
   //  .attr("d","M10,10L100,100") // d will be computed in the tick function.
    .attr("stroke","#000");
 
-  // var link = svg.append("g")
-  //     .attr("class", "links")
-  //   .selectAll("line")
-  //   .data(graph.links)
-  //   .enter().append("line")
-  //   .attr("stroke-width",0);
-      // .attr("stroke-width", function(d){return strokeWidth(d)})
-      // .style('marker-end', 'url(#end-arrow)' );
 
-
-  var dispName = function(d){
-    if (d.name !== undefined) {
-      if (d.group == "dead") {
-        return d.name+"#died:"+formatDateTime(d.t);
-      } else {
-        return d.name;
-      }
-    } else {
-      return d.id;
-    }
-  };
-
+   // draw node text
   var node = svg.append("g")
       .attr("class", "nodes")
     .selectAll("text")
@@ -197,25 +216,18 @@ d3.json("/heartbeat", function(error, dat) {
 
   node.exit().remove();
   nodec.exit().remove();
-  // link.exit().remove();
   path.exit().remove();
 
-  // node.append("title")
-  //     .text(function(d) { return d.id; });
-
+  // run simulation
   simulation
       .nodes(graph.nodes)
-      .on("tick", ticked);
-
+      .on("tick", ticked)
+      ;
   simulation.force("link")
-      .links(graph.links);
+      .links(graph.links)
+      ;
 
   function ticked() {
-    // link
-    //     .attr("x1", function(d) { return d.source.x; })
-    //     .attr("y1", function(d) { return d.source.y; })
-    //     .attr("x2", function(d) { return d.target.x; })
-    //     .attr("y2", function(d) { return d.target.y; });
     /*
      Let Vector V = P2 - P1, where P1 is the start position Vector
      and P2 the end position vector.
@@ -244,22 +256,7 @@ d3.json("/heartbeat", function(error, dat) {
     node
         .attr("x", function(d) { return d.x; })
         .attr("y", function(d) { return d.y; });
+
+
   }
 });
-
-function dragstarted(d) {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
-}
-
-function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
-}
-
-function dragended(d) {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
-}
